2c774cc4af2b1a9b9d3bdb22b04f5fbfe56c5436
zerocy18 <zerocy18@gmail.com>
2020-04-29 09:27:19 +0900

    같은 숫자는 싫어

---
+++ programmers/notdup.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    int tmp;
+    for(int i =0 ; i < arr.size(); i++){
+        answer.push_back(arr[i]);
+        if()
+    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}

e41bd49364124122f656d570fe11891624e727c4
zerocy18 <zerocy18@gmail.com>
2020-04-30 10:13:50 +0900

    문자열 내림차순으로 배치하기

---
+++ programmers/str_desc.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(char a, char b){
+    return a > b;
+}
+
+string solution(string s) {
+    string answer = "";
+    sort(s.begin(), s.end(),compare);
+    answer= s;
+    return answer;
+}

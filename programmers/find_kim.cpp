0f00c2c4dfe3a7a48830e00d2c1f399577597124
zerocy18 <zerocy18@gmail.com>
2020-04-30 10:38:53 +0900

    서울에서 김서방 찾기

---
+++ programmers/find_kim.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+    int index = 0;
+    for(int i = 0 ; i < seoul.size() ; i++){
+        if(seoul[i] == "Kim"){
+            index = i;
+            break;
+        }
+    }
+    answer = "김서방은 "+to_string(index)+"에 있다";
+    return answer;
+}
